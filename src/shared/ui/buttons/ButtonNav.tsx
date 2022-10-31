844f6642ce43a194fa5b5868bc498b2895a44489
Sergey Rovkach <srovk22@gmail.com>
2022-10-31 14:52:30 +0300

    fix: buttonNav link

--- src/shared/ui/buttons/ButtonNav.tsx
+++ src/shared/ui/buttons/ButtonNav.tsx
@@ -3,10 +3,10 @@ import Link from 'next/link'
 
 export const ButtonNav = ({ href, children, ...props }: { href: string; children: React.ReactNode; }) => {
   return (
-    <div className="flex-shrink-0 w-full px-8 sm:order-2 sm:w-auto flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-mainColor hover:bg-hoverColorBg hover:cursor-pointer font-bold">
-      <Link href={href}  {...props}>
+    <Link href={href}  {...props}>
+      <div className="flex-shrink-0 w-full px-8 sm:order-2 sm:w-auto flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-mainColor hover:bg-hoverColorBg hover:cursor-pointer font-bold">
         {children}
-      </Link>
-    </div>
+      </div>
+    </Link>
   );
 };
